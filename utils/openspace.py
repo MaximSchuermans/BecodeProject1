a1cac129ad2011f8a7fb47fcfa7ab0c590524953
Maxim Schuermans <schuermansmaxim1@gmail.com>
2024-10-24 09:49:25 +0200

    Boilerplate code for the openspace class

---
+++ utils/openspace.py
@@ -0,0 +1,20 @@
+from table.py import Table
+from typing import List
+
+class Openspace:
+
+    def __init__(self, tables: List[Tabel]) -> None:
+        self.tables = None
+        self.number_of_tables = None
+
+    def organize(self, names: List[str]) -> None:
+        pass
+
+    def display(self) -> None:
+        pass
+
+    def store(self, filename: str) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
