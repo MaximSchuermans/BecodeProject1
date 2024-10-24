e834acd7750317c17628434e362b3c2c9aed419e
Maxim Schuermans <schuermansmaxim1@gmail.com>
2024-10-24 11:56:24 +0200

    implemented read_xls function

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,8 +1,14 @@
-## DEPENDECY PANDAD read_excel file -> Write README.md:usage first before importing pandas
 from typing import List
+from pandas import read_excel, read_csv
 
 def read_xls(filename: str) -> List[str]:
     """
     Reads the excel file into a pandas dataframe and returns a list of names.
     """
-    pass
+    names = []
+    excel = read_excel(filename)
+    print("printing dataframe: ", excel)
+    for row in excel.itertuples():
+        names.append(row[1]) #Append the name to the names list
+    return names
+
