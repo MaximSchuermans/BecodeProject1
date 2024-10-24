445f6cbb044c34c6a9451ef72f052889c12a96cc
kevin87ops <Kevin.jamotte@gmail.com>
2024-10-24 10:31:56 +0200

    added descriptions of the methods and made it more readable

--- utils/table.py
+++ utils/table.py
@@ -1,21 +1,41 @@
 from module import whateveryouneed 
+
 class Seat:
     def __init__(self, free:bool, occupant:str ) -> None:
         self.free = free
         self.occupant = occupant
-        pass
+
     def set_occupant(self,name:str) -> None:
+        """
+        Allows the program to assign someone a seat if its free
+        """
         pass
+
     def remove_occupant(self) -> None:
+        """
+        remove someone from a seat and return the name of the person occupying the seat before
+        """
         pass
 
 class Table:
     def __init__(self, capacity:int)-> None:
         self.capacity = capacity
         self.seats = None
+
     def has_free_spot(self) ->bool:
+        """
+        returns a bool if a spot is available
+        """
         pass
+
     def assign_seat(self, name) -> None:
+        """
+        places someone at the table
+        """
         pass
+
     def left_capacity(self) -> int:
+        """
+        returns left seats with an integer
+        """
         pass
